2716954db6a9ed30c3572f52c50c0646f001fd0b
“Lucca <ge54gez@mytum.de>
2022-07-07 11:13:40 +0200

    Use Soundwave from spoken Text

---
+++ src/gui/gui.py
@@ -0,0 +1,27 @@
+"""GUI for our Assistant"""
+from tkinter import *
+from soundwave import stream
+
+# root Window
+root = Tk()
+root.title("Assistent3")
+root.geometry("400x200")
+
+#def matplotSoundwave():
+   #with soundwave.stream:
+   #     soundwave.plt.show()
+
+if __name__ == '__main__':
+    root.mainloop()
+
+# creating Labels of the Window
+# head = Label(root, text="Assistent3")
+# text = Label(root, text ="spoken Text is added here")
+
+# determining where labels are in window
+# head.grid(row=0, column=0)
+# text.grid(row=5, column=0)
+
+# creating Icon with Soundwave
+# button = ttk.Button(root, )
+# create main Eventloop
