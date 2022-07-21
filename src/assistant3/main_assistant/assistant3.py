186fc45816f64c9062c7a709f286f74bdde48866
Antonius Adisoemarta <ge29sew@mytum.de>
2022-07-21 02:53:57 +0200

    added weather internet and volume plugins

--- src/assistant3/main_assistant/assistant3.py
+++ src/assistant3/main_assistant/assistant3.py
@@ -45,11 +45,17 @@ class Assistant3():
         self.loc = processors.base_processor.Location()
         self.jok = processors.base_processor.Jokes()
         self.cal = processors.base_processor.Calculator()
+        self.int = processors.base_processor.Internet()
+        self.vol = processors.base_processor.Volume()
+        self.wet = processors.base_processor.Weather()
+
 
         # trigger plugin object
         self.trigger = processors.base_processor.TriggerPlugin()
         # the plugin_watcher object
-        self.plugin_watcher = PluginWatcher([self.wik, self.jok, self.loc, self.cal, self.mpp])
+        self.plugin_watcher = PluginWatcher(
+            [self.wik, self.jok, self.loc, self.cal, self.mpp, self.vol, self.wet, self.int]
+            )
         # optionaly adding a trigger Plugin ("hey assistant")
         self.plugin_watcher.add_trigger_plugin(self.trigger)
 
